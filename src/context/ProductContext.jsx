bdaca06563380dba0110821bd869816f76aea7fe
Yusuf <abdulsalamy49@gmail.com>
2026-09-07 19:53:33 +0100

    Fix import casing and add initialProducts.jsx

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -13,7 +13,7 @@ import { db } from "../firebase";
 import {
   categories,
   default as initialProducts,
-} from "../data/initialProducts.js";
+} from "../data/initialProducts.jsx";
 
 const ProductContext = createContext();
 
